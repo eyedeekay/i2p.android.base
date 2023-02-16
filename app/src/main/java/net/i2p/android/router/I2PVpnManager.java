c15092fd16456031fb48bf1df2a1a0001d285bcd
idk <hankhill19580@gmail.com>
2023-02-16 20:56:35 +0000

    add notes about license of files which include code copied from Orbot

--- app/src/main/java/net/i2p/android/router/I2PVpnManager.java
+++ app/src/main/java/net/i2p/android/router/I2PVpnManager.java
@@ -1,5 +1,23 @@
 package net.i2p.android.router;
 
+/* Note: provided because I copy-pasted much of this code from the equivalent
+ * module in Orbot. Apache2 is actually the default license for this project also.
+ *
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import android.annotation.TargetApi;
 import android.app.Application;
 import android.app.PendingIntent;
